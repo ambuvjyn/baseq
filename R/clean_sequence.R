3df91f6c048dec26f29f4df42e08d5238db35f9b
ambuvjyn <ambuvjyn@gmail.com>
2023-03-20 02:48:34 +0530

    v0.1.0.0

--- R/clean_sequence.R
+++ R/clean_sequence.R
@@ -6,31 +6,30 @@
 #' @param type a character string indicating the type of sequence ("DNA" or "RNA")
 #' @return a cleaned DNA/RNA sequence string
 #'
-#' @examples#'
+#' @examples
 #' sequence <- "ATGACTGCNATGCNCTAG"
 #' clean_sequence(sequence, type = "DNA")
-#' [1] "ATGACTGCATGCCTAG"
+#' # [1] "ATGACTGCATGCCTAG"
 #'
 #' sequence <- "AUGACUGCNAUGCNCUAG"
 #' clean_sequence(sequence, type = "RNA")
-#' [1] "AGACGCAGCCAG"
-#'
+#' # [1] "AUGACUGCAUGCCUAG"
 #' @export
 #' @importFrom stringr str_replace_all
 #' @importFrom stringr str_to_upper
 clean_sequence <- function(sequence, type = "DNA") {
-  #' Convert sequence to upper case
+  # Convert sequence to upper case
   sequence <- stringr::str_to_upper(sequence)
   if (type == "DNA") {
-    #' Remove any characters that are not A, C, G, T
+    # Remove any characters that are not A, C, G, T
     sequence <- stringr::str_replace_all(sequence, "[^ACGT]", "")
   } else if (type == "RNA") {
-    #' Remove any characters that are not A, C, G, U
+    # Remove any characters that are not A, C, G, U
     sequence <- stringr::str_replace_all(sequence, "[^ACGU]", "")
   } else {
-    #' Stop execution if type is not "DNA" or "RNA"
+    # Stop execution if type is not "DNA" or "RNA"
     stop("Invalid sequence type. Please specify 'DNA' or 'RNA'.")
   }
-  #' Return the cleaned sequence
+  # Return the cleaned sequence
   return(sequence)
 }
