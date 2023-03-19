f9a4f420c0c835b9c68df9ad0d80fea4b2108804
ambuvjyn <ambuvjyn@gmail.com>
2023-03-20 03:38:28 +0530

    v1.0

--- R/rna_to_dna.R
+++ R/rna_to_dna.R
@@ -1,17 +1,17 @@
 #' Reverse Transcription of a RNA sequence
 #'
-#' This function takes a DNA sequence as input and returns its RNA transcript.
+#' This function takes a RNA sequence as input and returns its DNA transcript.
 #'
-#' @param sequence A character string representing a DNA sequence.
-#' @return A character string representing the RNA transcript of the input DNA sequence.
+#' @param sequence A character string representing a RNA sequence.
+#' @return A character string representing the RNA transcript of the input RNA sequence.
 #' @examples
 #' sequence <- "AUCGAGCUAGCUAGCUAGCUAGCU"
 #' rna_to_dna(sequence)
 #' # Returns "ATCGAGCTAGCTAGCTAGCTAGCT"
 #' @export
 rna_to_dna <- function(sequence) {
-  # Convert the sequence to RNA by replacing T with U
+  # Convert the sequence to DNA by replacing U with T
   transcript <- gsub("U", "T", sequence)
-  # Return the RNA transcript
+  # Return the DNA transcript
   return(transcript)
 }
